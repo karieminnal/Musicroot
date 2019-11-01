006d7c8b6c1ee8aa3e8697301150fcec855bb525
karieminnal <karieminnal@gmail.com>
2019-11-01 12:34:48 +0700

    issues #4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # importing speech recognition package from google api 
 import speech_recognition as sr 
 import playsound # to play saved mp3 file 
-from gtts import gTTS # google text to speech 
+from gtts import gTTS #4
 import os # to save/open files 
 import wolframalpha # to calculate strings into formula 
 from selenium import webdriver # to control browser operations 
